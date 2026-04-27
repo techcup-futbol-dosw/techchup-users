836f8a9af688838d10f2d34ed729f9bca3699010
brayan loaiza leal <alvarezdairo9@gmail.com>
2026-04-26 21:25:23 -0500

    fix : corregir configuración de JaCoCo, pipeline y dependencias

--- src/main/java/edu/dosw/users/App.java
+++ src/main/java/edu/dosw/users/App.java
@@ -1,10 +1,11 @@
 package edu.dosw.users;
 
-/**
- * Hello world!
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
     }
 }
--- src/test/java/edu/dosw/users/AppTest.java
+++ src/test/java/edu/dosw/users/AppTest.java
@@ -1,19 +1,12 @@
 package edu.dosw.users;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest {
+@SpringBootTest
+class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    void contextLoads() {
     }
 }
